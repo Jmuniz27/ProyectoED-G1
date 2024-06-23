e3f33ea12c84202df7a32e9324a8e72092baeabd
isabellaim <iimartin@espol.edu.ec>
2024-06-23 13:17:47 -0500

    Mejoras a crearVehiculo

--- src/main/java/ec/edu/espol/proyectoed_g1/CrearVehiculoController.java
+++ src/main/java/ec/edu/espol/proyectoed_g1/CrearVehiculoController.java
@@ -4,6 +4,8 @@
  */
 package ec.edu.espol.proyectoed_g1;
 
+import ec.edu.espol.proyectoed_g1.modelo.Interfaces.List;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -14,6 +16,9 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
 /**
  * FXML Controller class
@@ -45,12 +50,6 @@ public class CrearVehiculoController implements Initializable {
     @FXML
     private CheckBox checkNegociable;
     @FXML
-    private CheckBox checkAccidente;
-    @FXML
-    private TextArea taAccidente;
-    @FXML
-    private TextArea taMantenimiento;
-    @FXML
     private TextField tfNombre;
     @FXML
     private TextField tfApellido;
@@ -60,6 +59,30 @@ public class CrearVehiculoController implements Initializable {
     private TextField tfCorreo;
     @FXML
     private Button botonPonerVenta;
+    @FXML
+    private ImageView imgLogo;
+    @FXML
+    private CheckBox cCarroceria;
+    @FXML
+    private CheckBox cParachoques;
+    @FXML
+    private CheckBox cSuspension;
+    @FXML
+    private CheckBox cFarros;
+    @FXML
+    private CheckBox cEscape;
+    @FXML
+    private CheckBox cAceite;
+    @FXML
+    private CheckBox cFiltros;
+    @FXML
+    private CheckBox cBateria;
+    @FXML
+    private CheckBox cFrenos;
+    @FXML
+    private CheckBox cNeumaticos;
+    @FXML
+    private Button botonSubirImagen;
 
     /**
      * Initializes the controller class.
@@ -73,12 +96,54 @@ public class CrearVehiculoController implements Initializable {
     private void irInicio(ActionEvent event) {
     }
 
-    @FXML
-    private void clickEnAccidente(ActionEvent event) {
-    }
 
     @FXML
     private void clickEnPonerVenta(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickEnCCarroceria(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickEnCParachoques(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickEnCSuspension(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickEnCFaros(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickEnCbEscape(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickEnCAceite(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickEnCFiltros(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickEnCBateria(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickEnCFrenos(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickEnCNeumaticos(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickEnSubirImagen(ActionEvent event) {
+        
     }
     
 }
